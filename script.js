dc41c9959f1cc834d2ab17a16b62911fcfff3ed3
Lucas Vieira <160544535+LucasVieira4@users.noreply.github.com>
2024-05-17 17:09:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('mouseover', function(){
+            card.style.transform = 'scale(1.1)';
+            card.style.transition = 'transform 0.3s ease';
+        });
+        card.addEventListener('mouseout', function() {
+            card.style.transform = 'scale(1)';
+            card.style.transition = 'transform 0.3s ease';
+        });
+    });
+});
